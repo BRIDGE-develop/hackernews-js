975f72dfcca471a0aebf83f29137076e703289e2
silentnara <31914090+silentnara@users.noreply.github.com>
2018-12-22 14:58:32 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null,
+      searchTerm:DEFAULT_QUERY,
+    }
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result){
+    this.setState({result});
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
+
+  render() {
+    const { searchTerm, result } = this.state;
+    
+    if (!result) { return null; }
+
+    return (
+      <div className="page">
+        <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
